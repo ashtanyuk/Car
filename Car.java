905b14303557dc8cd6f88b250e29a3a6239ddd2f
Anton Shtanyuk <ashtanyuk@gmail.com>
2022-02-01 18:35:23 +0300

    Начало

---
+++ Car.java
@@ -0,0 +1,49 @@
+public class Car {
+    int speed;
+    int odometer;
+    int time;
+    int fuel;
+
+    public Car(int speed, int fuel) {
+        if(speed<0)
+            this.speed = 0;
+        else
+            this.speed = speed;
+        odometer = 0;
+        time = 0;
+        this.fuel = fuel;
+    }
+    public void sayState() {
+        if(speed > 0)
+            System.out.println("Еду");
+        else
+            System.out.println("Стою");
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public void gaz() {
+        speed += 5;
+    }
+    public void tormoz() {
+        speed -= 5;
+        if(speed < 0)
+            speed = 0;
+    }
+    public void step() {
+        odometer += speed;
+        time++;
+    }
+    public double average() {
+        return (double)odometer/time;
+    }
+    public int km() {
+        return odometer;
+    }
+    public void sayKm() {
+        System.out.println(odometer);
+    }
+    public void sayAv() {
+        System.out.println(average());
+    }
+}
